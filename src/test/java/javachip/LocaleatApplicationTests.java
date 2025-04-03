3e8d0404acb16d17163e3b4504f20e72737a532d
Yeojin0523 <d02jyjeong@naver.com>
2025-04-03 18:41:37 +0900

    develop 브랜치 첫 커밋

---
+++ src/test/java/javachip/LocaleatApplicationTests.java
@@ -0,0 +1,13 @@
+package javachip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocaleatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
