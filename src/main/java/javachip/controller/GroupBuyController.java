e8cdb9619b3be44529ba81b1ffb9255a5f5191f1
Wish <asgvcx@naver.com>
2025-05-17 00:55:59 +0900

    feat: 공동구매상세조회 개발
    related to #14

--- src/main/java/javachip/controller/GroupBuyController.java
+++ src/main/java/javachip/controller/GroupBuyController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import javachip.dto.GroupBuyParticipationRequest;
 import javachip.dto.groupbuy.GroupBuyCreateRequest;
 import javachip.dto.groupbuy.GroupBuyCreateResponse;
+import javachip.dto.groupbuy.GroupBuyDetailResponse;
 import javachip.service.GroupBuyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 public class GroupBuyController {
 
     private final GroupBuyService groupBuyService;
+    private final GroupBuyService gbService;
 
     /**
      * 공동구매 생성 (소비자)
@@ -37,5 +39,12 @@ public class GroupBuyController {
             @RequestHeader("X-USER-ID") String userId) {
         groupBuyService.participateInGroupBuy(request.getGroupBuyId(), userId, request.getQuantity());
         return ResponseEntity.ok().build();
+    }
+
+    //상세조회
+    @GetMapping("/{id}")
+    public ResponseEntity<GroupBuyDetailResponse> getDetail(@PathVariable("id") Long id) {
+        GroupBuyDetailResponse resp = gbService.getDetail(id);
+        return ResponseEntity.ok(resp);
     }
 }
