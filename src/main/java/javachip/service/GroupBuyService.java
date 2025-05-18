5ed2c0bc26cf9c8d1622311aecde817ab4aec256
k-mha <130807104+k-mha@users.noreply.github.com>
2025-05-18 20:29:05 +0900

    Fix: 서비스 파일 경로 수정

--- src/main/java/javachip/service/GroupBuyService.java
+++ src/main/java/javachip/service/GroupBuyService.java
@@ -4,14 +4,12 @@ import javachip.dto.groupbuy.*;
 import javachip.entity.*;
 import javachip.repository.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
