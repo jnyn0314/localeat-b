6aa6ae9e294f3f5f7360a9ba475ea5535dcc72f6
k-mha <130807104+k-mha@users.noreply.github.com>
2025-05-23 22:04:53 +0900

    Feat: 판매자 주문관리 기능 구현

--- src/main/java/javachip/repository/OrderItemRepository.java
+++ src/main/java/javachip/repository/OrderItemRepository.java
@@ -10,6 +10,10 @@ import javachip.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    List<OrderItem> findByProductSellerUserId(String userId);
+
 }
